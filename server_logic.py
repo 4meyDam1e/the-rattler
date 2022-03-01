7cd3f65cc755c838b4bfb5f31952db0f0b377d22
destructa321 <79952127+Destructa321@users.noreply.github.com>
2022-03-01 20:31:48 +0000

    Added functions to avoid walls and own body.

--- server_logic.py
+++ server_logic.py
@@ -99,7 +99,7 @@ def choose_move(data: dict) -> str:
 
     # TODO: uncomment the lines below so you can see what this data looks like in your output!
     # print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
-    # print(f"All board data this turn: {data}")
+    print(f"All board data this turn: {data}")
     # print(f"My Battlesnakes head this turn is: {my_head}")
     # print(f"My Battlesnakes body this turn is: {my_body}")
 
